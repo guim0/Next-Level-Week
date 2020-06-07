3d9362bdced03a980ce9744194b89f37e64c2c48
guim0 <guilherme.moraes2407@gmail.com>
2020-06-07 17:04:07 -0300

    integração de API

---
+++ scripts/intro.js
@@ -0,0 +1,62 @@
+// comments
+// document.write("Hello")
+
+// variaveis, tipos de dados
+// let myvar = "He"
+// const myconst = "He"
+
+// STRING
+//"Isso é uma string"
+//'Isso tambem é uma string'
+//`Isso tambem é uma string `
+
+//tipos de dados
+//NUMBER
+//const n1=1
+//const n2=12
+//document.write(n1+n2)
+//BOOLEANS
+//const bTrue = true
+//const bFalse = false
+//document.write(bFalse)
+
+// objetos possuem propriedade e funcionalidades
+// const pessoa = {
+//  altura: "1.80".
+//  idade: 24,
+//  solteiro: true,
+//  correr(){
+//      document.write("executar uma grande logica de correr")
+//  }
+//}
+
+//pessoa.correr()
+
+//Arrays - Vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name:"My Name"}]
+// document.write(colecaoDeBolinhas[3].name)
+
+//  FUNÇÕES - funcionalidades - Atalhos - Reuso de código
+
+// registrar função
+// function sayMyName(name){
+//  document.write(name)
+//}
+//      EXECUTAR
+//sayMyName("Guilherme")
+//sayMyName("Caio")
+//sayMyName("Lucas")
+
+//CONDICIONAIS
+// const notaFinal = 8
+
+//if (notaFinal <5){
+//    document.write("Reprovado")
+//} else{
+//    documenet.write("Aprovado")
+//}
+
+// LOOP REPETIÇÃO
+//for (i = 0; i < 10; i++)
+// document.write('<p>${i}</p>')
+//}
